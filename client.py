03fd8dd0d5014a1c70f2adac8f6026e407a858b3
elliejan99 <eijan@uci.edu>
2019-11-10 10:54:25 -0800

    added gitignore

--- client.py
+++ client.py
@@ -1,9 +1,10 @@
 import Algorithmia
+import json
 from Algorithmia.acl import ReadAcl, AclType
 
 apiKey = "simglFzyXkye8lDWJ0rGViwralu1"
 # Create the Algorithmia client
-client = Algorithmia.client(simglFzyXkye8lDWJ0rGViwralu1)
+client = Algorithmia.client("simglFzyXkye8lDWJ0rGViwralu1")
 
 # Set your Data URI
 nlp_directory = client.dir("data://eijan/nlp_directory")
@@ -19,7 +20,7 @@ acl.read_acl == AclType.my_algos  # True
 nlp_directory.update_permissions(ReadAcl.private)
 nlp_directory.get_permissions().read_acl == AclType.private # True
 
-text_file = "data://eijan/nlp_directory/jack_london.txt"
+text_file = "data://eijan/nlp_directory/data1.txt"
 
 # Upload local file
 if client.file(text_file).exists() is False:
@@ -27,10 +28,13 @@ if client.file(text_file).exists() is False:
 
 # Download contents of file as a string
 if client.file(text_file).exists() is True:
-    input = client.file(text_file).getString()
-
+    #input_string = client.file(text_file).getString()
+    with open('data1.txt') as json_file:
+        input = json.load(json_file)
+    
 # Create the algorithm object using the Summarizer algorithm
-algo = client.algo("nlp/Summarizer/0.1.3")
+algo = client.algo('matching/DatingAlgorithm/0.1.3')
+#algo_dict = json.loads(algo.pipe(input).result)
 # Pass in input required by algorithm
 try:
     # Get the summary result of your file's contents
